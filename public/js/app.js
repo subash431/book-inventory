7b6788c27e6570ee027434ad629d48ecc17be3da
subash pariyar <pariyarsubash2055@gmail.com>
2024-10-01 20:49:45 -0400

    updating fetch all book button

--- public/js/app.js
+++ public/js/app.js
@@ -25,3 +25,29 @@ document
         });
       });
   });
+
+document
+  .getElementById("fetchBooksBtn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    fetch(`/books/all`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const tbody = document.querySelector("#booksList tbody");
+        tbody.innerHTML = "";
+        data.forEach((book) => {
+          const row = `
+            <tr>
+              <td>${book.title}</td>
+              <td>${book.author}</td>
+              <td>${book.genre}</td>
+              <td>${book.publication_date}</td>
+              <td>${book.isbn}</td>
+            </tr>
+          `;
+          tbody.insertAdjacentHTML("beforeend", row);
+        });
+      });
+  });
